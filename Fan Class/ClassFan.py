ad2a844ae8e939140dadb6dde2b8c57c09bb61c8
laivwxyz <delacruzlailanie2004@gmail.com>
2023-07-01 23:05:41 +0800

    Create constructor that creates a fan with the specified speed, radius, color and on

--- Fan Class/ClassFan.py
+++ Fan Class/ClassFan.py
@@ -4,3 +4,6 @@ class Fan:
     SLOW = 1
     MEDIUM = 2
     FAST = 3
+
+    # Create constructor that creates a fan with the specified speed, radius, color and on
+    def __init__(self, speed = SLOW, radius = 5, color = 'blue', on = False):
