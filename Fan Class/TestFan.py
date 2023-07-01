349e427ef364dea4c8bb428ceb2bc8a2d56073bf
laivwxyz <delacruzlailanie2004@gmail.com>
2023-07-01 23:15:14 +0800

    Create the first Fan object

--- Fan Class/TestFan.py
+++ Fan Class/TestFan.py
@@ -1,2 +1,14 @@
 # import class Fan from ClassFan.py
 from ClassFan import Fan
+
+def test_fan():
+    # Create the first Fan object
+    fan1 = Fan()
+    fan1.get_speed()
+    fan1.set_speed(Fan.FAST)
+    fan1.get_on()
+    fan1.set_on(True)
+    fan1.get_radius()
+    fan1.set_radius(10)
+    fan1.get_color()
+    fan1.set_color("Yellow")
