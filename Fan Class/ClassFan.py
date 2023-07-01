295d5da778df56ba565a3fbc863e3907ace65d52
laivwxyz <delacruzlailanie2004@gmail.com>
2023-07-01 23:11:04 +0800

    Create mutator (setter) method for color

--- Fan Class/ClassFan.py
+++ Fan Class/ClassFan.py
@@ -40,3 +40,7 @@ class Fan:
     # Create accessor (getter) method for color
     def get_color(self):
         return self.__color
+    
+    # Create mutator (setter) method for color
+    def set_color(self, color):
+        self.__color = color
