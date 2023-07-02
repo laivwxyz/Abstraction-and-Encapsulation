ba68ba8cc39fc0b7bf941be2dc8f607df96a5858
laivwxyz <delacruzlailanie2004@gmail.com>
2023-07-02 20:18:15 +0800

    Run the test_pet function

--- Pet Class/TestPet.py
+++ Pet Class/TestPet.py
@@ -15,3 +15,6 @@ def test_pet():
     print("Name:", pet.get_name())
     print("Animal Type:", pet.get_animal_type())
     print("Age:", pet.get_age())
+    
+# Run the test_pet function
+test_pet()
