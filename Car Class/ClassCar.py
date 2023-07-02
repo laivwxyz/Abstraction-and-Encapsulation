4e265fcda5b25111ba306a03322320530cd5900e
laivwxyz <delacruzlailanie2004@gmail.com>
2023-07-02 12:33:33 +0800

    Create constructor that creates a car with the specified year model, make and speed

--- Car Class/ClassCar.py
+++ Car Class/ClassCar.py
@@ -1,2 +1,4 @@
 # Create a class named Car
 class Car:
+    # Create constructor that creates a car with the specified year model, make and speed
+    def __init__(self, year_model, make, speed = 0):
