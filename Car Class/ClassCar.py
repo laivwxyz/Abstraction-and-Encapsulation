63c36b2101a1a3072c280c022a238e4e076d3b2f
laivwxyz <delacruzlailanie2004@gmail.com>
2023-07-02 12:34:25 +0800

    Create instance variable

--- Car Class/ClassCar.py
+++ Car Class/ClassCar.py
@@ -2,3 +2,7 @@
 class Car:
     # Create constructor that creates a car with the specified year model, make and speed
     def __init__(self, year_model, make, speed = 0):
+        # Create instance variable 
+        self.__year_model = year_model
+        self.__make = make
+        self.__speed = speed
