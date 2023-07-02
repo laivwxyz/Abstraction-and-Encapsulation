bf23f8d0183a7cfe1f6fdf3030c6bc2ccf03955b
laivwxyz <delacruzlailanie2004@gmail.com>
2023-07-02 13:44:14 +0800

    Call the accelerate method five times and display the current speed

--- Car Class/TestCar.py
+++ Car Class/TestCar.py
@@ -4,3 +4,12 @@ from ClassCar import Car
 def test_car():
     # Create a Car object
     car = Car(2022, "Toyota", 0)
+
+    # Call the accelerate method five times and display the current speed
+    print("Accelerate")
+    print("Car year model: ", car.get_year_model())
+    print("Car is made of: ", car.get_make())
+    print("Speed")
+    for _ in range(5):
+        car.accelerate()
+        print("Current speed:", car.get_speed())
